a4ae03d3a4315a0343b030e04eff45af9d7295d7
mr Yacine <mryacine@MacBook-Air-de-mr.local>
2018-10-30 23:11:10 +0100

    Speacher

--- speacker/ViewController.swift
+++ speacker/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
-
+    @IBAction func speach(_ sender: UIButton) {
+        myUtterance = AVSpeechUtterance(string: textView.text)
+        myUtterance.rate = 0.3
+        synth.speak(myUtterance)
+    }
+    
 }
 
